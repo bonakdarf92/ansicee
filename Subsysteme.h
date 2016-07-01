beca881bfb4128f080a54a6b8b1e11e230b3112e
faridbonakdar <bonakdarf92@googlemail.com>
2016-07-01 12:06:50 +0200

    Erste Erstellung der Codes

---
+++ Subsysteme.h
@@ -0,0 +1,10 @@
+//
+// Created by Farid Bonakdar on 30.06.16.
+//
+
+#ifndef ANSICEE_SUBSYSTEME_H
+#define ANSICEE_SUBSYSTEME_H
+
+double subsystemeBerechnen(double n_MC);
+
+#endif //ANSICEE_SUBSYSTEME_H
---
+++ modelAntrieb.h
@@ -0,0 +1,12 @@
+//
+// Created by Farid Bonakdar on 30.06.16.
+//
+
+#ifndef ANSICEE_MODELANTRIEB_H
+#define ANSICEE_MODELANTRIEB_H
+
+double berechneMatrixC();
+
+double berechneMatrixD();
+
+#endif //ANSICEE_MODELANTRIEB_H
